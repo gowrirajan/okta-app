b77cdb634be6a17d6f2edb618e8f91b9dcbb80e2
Rajan <gowri.rajan@hidglobal.com>
2024-11-21 17:24:51 +0530

    renamed credentials

---
+++ oktaApp/src/test/java/com/example/oktaApp/OktaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oktaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
